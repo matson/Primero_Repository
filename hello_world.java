76d2c8bbe2e01ba33389e9bc778e6f17577be382
Matson <madison.k.adams@gmail.com>
2020-01-21 10:51:16 -0500

    Create hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+   public static void main(String[] args) {
+    	
+    	System.out.println("Hello World!");//the Java way
+      //System.console.write()
+      //this is C# way 
+      
+    }
+  
+}
